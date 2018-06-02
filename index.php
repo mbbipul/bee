82f5cec06af6956448234509a1af0afaab388ed5
mbbipul <30537029+mbbipul@users.noreply.github.com>
2018-06-02 15:19:40 +0600

    Navbar

--- index.php
+++ index.php
@@ -6,12 +6,35 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="assets/css/materialize.min.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="assets/css/bee.css">
+        <!--import w3css -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <!--Import bootstrap-->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
+
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="navbar-fixed" style="max-height: 49px">
+            <nav>
+              <div class="nav-wrapper">
+                <a href="#!" class="brand-logo bee-margin-left-168">Logo</a>
+                <ul class="right hide-on-med-and-down">
+                  <li><a href="sass.html">Sass</a></li>
+                  <li><a href="badges.html">Components</a></li>
+                </ul>
+              </div>
+            </nav>
+        </div>
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="assets/js/materialize.min.js"></script>
     </body>
 </html>
