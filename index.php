710734f9c0d2e974f86aa62bada5191b9381fdcc
mbbipul <30537029+mbbipul@users.noreply.github.com>
2018-06-04 02:36:01 +0600

    Navbar with Search and mdb setup

--- index.php
+++ index.php
@@ -13,11 +13,14 @@ and open the template in the editor.
         <link rel="stylesheet" href="assets/css/bee.css">
         <!--import w3css -->
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="assets/css/mdb.min.css">
         <!--Import bootstrap-->
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
@@ -25,16 +28,35 @@ and open the template in the editor.
     <body>
         <div class="navbar-fixed" style="max-height: 49px">
             <nav>
-              <div class="nav-wrapper">
-                <a href="#!" class="brand-logo bee-margin-left-168">Logo</a>
-                <ul class="right hide-on-med-and-down">
-                  <li><a href="sass.html">Sass</a></li>
-                  <li><a href="badges.html">Components</a></li>
-                </ul>
-              </div>
+                <div class="nav-wrapper">
+                    <a class="navbar-brand bee-margin-left-168 brand-logo "  href="#">
+                        <img src="assets/beeImage/beeLogo1.png" width="70px" height="30px" alt="">
+                    </a>
+                    
+                    <form class="form-inline bee-margin-left-256 bee-width-200 " >
+    <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search">
+    <i class="fa fa-search" aria-hidden="true"></i>
+</form>
+
+                    <ul class="right hide-on-med-and-down">
+                   
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                    </ul>
+
+    
+                    
+                </div>
             </nav>
         </div>
         <!--JavaScript at end of body for optimized loading-->
         <script type="text/javascript" src="assets/js/materialize.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="assets/js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="assets/js/mdb.min.js"></script>
     </body>
 </html>
