cfa855939a47045d2c277d572507ecdfdf01d971
mbbipul <30537029+mbbipul@users.noreply.github.com>
2018-07-07 10:03:54 +0600

    Designing signup and signin page with php

--- index.php
+++ index.php
@@ -1,152 +1,321 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="assets/css/materialize.min.css"  media="screen,projection"/>
-        <link rel="stylesheet" href="assets/css/bee.css">
-        <!--import w3css -->
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="assets/css/mdb.min.css">
-        <!--Import bootstrap-->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
-        <!-- Bootstrap core CSS -->
-        <link href="assets/css/style.css" rel="stylesheet">
+<?php
+if(isset($_SESSION['id']))
+{
+  header("Location: home.php");
+}
+if(isset($_COOKIE['A']) && isset($_COOKIE['B']))
+   {
+    include('php/db.php');
+    include('php/class.php');
+    $q->cookie_login($_COOKIE['A'],$_COOKIE['B']);
+   }
+
+?>
+
+	<!DOCTYPE html>
+	<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		
+		<link rel="icon" href="beeImage/beeLogo1.png">
+
+		<title>bee</title>
+
+                <link href="assets/css/bootstrap_2.css" rel="stylesheet">
+
+                <link href="assets/css/cover.css" rel="stylesheet">
+
+		
+		<style>
+			#btn-env:hover {
+				color: #10ad9d;
+			}
+			
+			#btn-env {
+				color: #0bb320;
+			}
+		</style>
+
+	</head>
+
+	<body onload="load();">
+		<div class="modal fade" id="notice" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h3 class="modal-title" id="myModalLabel" style="color: rgb(172, 19, 19);">IMPORTANT THINGS NEED TO KNOW</h3>
+					</div>
+					<div class="modal-body">
+
+						<div class="row">
+							<div class="col-md-12">
+								<h4 class="alert alert-danger">
+                            You can not change your primary email after signing up. So please provide a working email id of you.
+                            </h4>
+
+								<h4 class="alert alert-info">
+                            It is our request to you to provide <b>your name </b>correctly and check it before submitting.<br>
+                            Remember you never can change your name after this signing up process as it is your identity.
+                            </h4>
+							</div>
+
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="site-wrapper">
+
+			<div class="site-wrapper-inner">
+
+				<div class="cover-container">
+
+					<div class="masthead clearfix">
+						<div class="inner">
+                                                    <h3 class="masthead-brand"><a href="index.php" style="color: #ffffff;">bee</a></h3>
+							<nav>
+								<ul class="nav masthead-nav">
+									<li onclick="signup();" id="btnsign"><a>Sign Up</a></li>
+									<li onclick="login();" id="btnlog"><a>Log In</a></li>
+								
+								</ul>
+							</nav>
+						</div>
+					</div>
+
+					<div class="inner cover bar" id="signup" style="font-size: larger;">
+						<div id="success"></div>
+						<img src="img/loader.gif" id="loader" height="25%" style="position: fixed;margin: 0 auto;z-index: 400;">
 
 
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+						<form class="form-horizontal" method="post" action="php/signup.php" id="sign_up_form">
+							<br>
+							<h1 class="cover-heading">Create Account
+                   
+                  </h1>
+							<div class="form-group">
 
-    <body>
-        <div class="navbar-fixed" style="max-height: 49px">
-            <nav class="cyan darken-4">
-                <div class="nav-wrapper">
-                    <a class="navbar-brand bee-margin-left-168 brand-logo "  href="#">
-                        <img src="assets/beeImage/beeLogo1.png" width="70px" height="30px" alt="">
-                    </a>
+								<div class="col-sm-12">
+									<input type="email" onblur="check_email();" autocomplete="off" class="form-control" value="<?php if(isset($_POST['signupEmail'])) echo $_POST['signupEmail']; ?>" id="signupEmail" name="signupEmail" placeholder="  Type your working Email..." required>
+									<span id="email-info"></span>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-6">
+									<input type="text" class="form-control" autocomplete="off" id="firstname" value="<?php if(isset($_POST['firstname'])) echo $_POST['firstname']; ?>" name="firstname" placeholder="  First Name" required>
+								</div>
+								
+								<div class="col-sm-6">
+									<input type="text" class="form-control" autocomplete="off" id="lastname" value="<?php if(isset($_POST['lastname'])) echo $_POST['lastname']; ?>" name="lastname" placeholder="  Last Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputSex" class="col-sm-2 control-label">I am</label>
+								<div class="col-sm-4">
+									<label class="radio-inline">
+										<input type="radio" name="sex" id="sex1" value="male" required> <b>Boy</b>
+									</label>
+									<label class="radio-inline">
+										<input type="radio" name="sex" id="sex2" value="female" checked="checked" required> <b>Girl</b>
+									</label>
+								</div>
+							</div>
 
-                    <form class="form-inline bee-margin-left-256 bee-width-200 " >
-                        <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search">
-                        <i class="fa fa-search" aria-hidden="true"></i>
-                    </form>
+							<div class="form-group">
 
-                </div>
-            </nav>
-        </div>
-        <div class="grey lighten-4">
-            <br>
-            <div class="row bee-margin-top-168">
+								<div class="col-sm-6">
+									<input type="password" class="form-control" id="password1" name="password" placeholder="Password" required>
+								</div>
+								<div class="col-sm-6">
+									<input type="password" class="form-control" id="password2" placeholder="Confirm Password" required>
+								</div>
+								<span id="password-info"></span>
+							</div>
 
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    <div >
-                            <div class="box image">
-      <div class="box-header">
-        <h3><a href=""><img src="https://goo.gl/oOD0V2" alt="" />Roswell Parian</a>
-          <span>March 21,18:45pm <i class="fa fa-globe"></i></span>
-        </h3>
-        <span><i class="fa fa-angle-down"></i></span>
-        <div class="window"><span></span></div>
-      </div>
-      <div class="box-content">
-        <div class="content">
-          <img src="https://goo.gl/nvqkEz" alt="" />
-        </div>
-        <div class="bottom">
-          <p>The life is much more interesant, when you remmenber the last time</p>
-          <span><span class="fa fa-search-plus"></span></span>
-        </div>
-      </div>
-      <div class="box-likes">
-        <div class="row">
-          <span><a href="#"><img src="https://goo.gl/oM0Y8G" alt="" /></a></span>
-          <span><a href="#"><img src="https://goo.gl/vswgSn" alt="" /></a></span>
-          <span><a href="#"><img src="https://goo.gl/4W27eB" alt="" /></a></span>
-          <span><a href="#">+99</a></span>
-          <span>Like this</span>
-        </div>
-        <div class="row">
-          <span>145 comments</span>
-        </div>
-      </div>
-      <div class="box-buttons">
-        <div class="row">
-          <button><span class="fa fa-thumbs-up"></span></button>
-          <button><span class="ion-chatbox-working"></span></button>
-          <button><span class="ion-share"></span></button>
-        </div>
-      </div>
-      <div class="box-click"><span><i class="ion-chatbox-working"></i> View 140 more comments</span></div>
-      <div class="box-comments">
-        <div class="comment"><img src="https://goo.gl/oM0Y8G" alt="" />
-          <div class="content">
-            <h3><a href="">Emily Rudd</a><span><time> 1 hr - </time><a href="#">Like</a></span></h3>
-            <p>Wow irealy, i love here. Nice pic</p>
-          </div>
-        </div>
-        <div class="comment"><img src="https://goo.gl/vswgSn" alt="" />
-          <div class="content">
-            <h3><a href="">barbara Palvin</a><span><time> 1 hr - </time><a href="#">Like</a></span></h3>
-            <p>The life is perfect, <3 Nice Scene</p>
-          </div>
-        </div>
-        <div class="comment"><img src="https://goo.gl/4W27eB" alt="" />
-          <div class="content">
-            <h3><a href="">Erica Mohn</a><span><time> 1 hr - </time><a href="#">Like</a></span></h3>
-            <p>Keep up, look pro :D</p>
-          </div>
-        </div>
-        <div class="comment"><img src="https://goo.gl/zX8wpb" alt="" />
-          <div class="content">
-            <h3><a href="">karlie Kloss</a><span><time> 1 hr - </time><a href="#">Like</a></span></h3>
-            <p>I like the scene & the thing <3 lol</p>
-          </div>
-        </div>
-        <div class="comment"><img src="https://goo.gl/K7E2k4" alt="" />
-          <div class="content">
-            <h3><a href="">Candice Swanepoel</a><span><time> 1 hr - </time><a href="#">Like</a></span></h3>
-            <p>The simple idea is everything, :) oooh</p>
-          </div>
-        </div>
-      </div>
-        <div class="box-new-comment">
-          <img src="https://goo.gl/oOD0V2" alt="" />
-          <div class="content">
-            <div class="row">
-              <textarea placeholder="write a comment..."></textarea>
-            </div>
-            <div class="row">
-              <span class="ion-android-attach"></span>
-              <span class="fa fa-smile-o"></span>
-            </div>
-          </div>
-        </div>
-    </div>
-                    </div>
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-            </div>
-        </div>
-        <!--JavaScript at end of body for optimized loading-->
-        <script type="text/javascript" src="assets/js/materialize.min.js"></script>
-        <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
-        <!-- Bootstrap tooltips -->
-        <script type="text/javascript" src="assets/js/popper.min.js"></script>
-        <!-- Bootstrap core JavaScript -->
-        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-        <!-- MDB core JavaScript -->
-        <script type="text/javascript" src="assets/js/mdb.min.js"></script>
-    </body>
-</html>
+							<button type="submit" class="btn btn-primary" value="Sign Up" id="signUpBtn" data-toggle="tooltip" data-placement="left" title="Click here to sign up"><span class="glyphicon glyphicon-plus"></span> Sign Up </button>
+
+						</form>
+						<br>
+					</div>
+					<div class="inner cover bar" id="login" style="font-size: larger">
+						<br>
+						<h1 class="cover-heading">Sign In</h1>
+						<form class="form-horizontal" method="post" id="login_form">
+							<div class="form-group">
+
+								<div class="col-sm-12">
+									<input type="email" class="form-control" autocomplete="off" id="loginEmail" name="loginEmail" placeholder="Email that you have used in the sign up process.." required>
+								</div>
+
+							</div>
+							<div class="form-group">
+
+								<div class="col-sm-12">
+									<input type="password" class="form-control" id="loginPassword" name="loginPassword" placeholder="Password" required>
+								</div>
+								<span id="pass_err"></span>
+							</div>
+
+							<div class="form-group">
+								<div class="col-sm-12">
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="special_key"> Keep me logged in
+										</label>
+									</div>
+								</div>
+							</div>
+							<button type="submit" id="loginBtn" class="btn btn-primary" value="Log in">Log in <span class="glyphicon glyphicon-user"></span></button>
+							<br>
+
+						</form>
+						<br>
+
+					</div>
+
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<!-- Bootstrap core JavaScript
+    ================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+                <script src="assets/js/jquery.js"></script>
+                <script src="assets/js/bootstrap.min.js"></script>
+		<script>
+			var loader = $('#loader');
+			loader.hide();
+			var signupdiv = $('#signup');
+			var logindiv = $('#login');
+			var aboutdiv = $('#about');
+
+			function signup() {
+				logindiv.hide();
+				aboutdiv.hide();
+				signupdiv.slideToggle("slow");
+				$('#btnsign').addClass("active");
+				$('#btnabout').removeClass('active');
+				$('#btnlog').removeClass("active");
+			}
+
+			function login() {
+				signupdiv.hide();
+				aboutdiv.hide();
+				logindiv.slideToggle("slow");
+				$('#btnlog').addClass("active");
+				$('#btnabout').removeClass('active');
+				$('#btnsign').removeClass("active");
+			}
+
+			function about() {
+				logindiv.hide();
+				signupdiv.hide();
+				aboutdiv.slideToggle("slow");
+				$('#btnsign').removeClass("active");
+				$('#btnlog').removeClass("active");
+				$('#btnabout').addClass('active');
+
+			}
+
+			function load() {
+				logindiv.hide();
+				aboutdiv.hide();
+				$('#btnsign').addClass("active");
+
+			}
+
+			function check_email() {
+
+				$('#email-info').html('Loading...');
+				var email = $('#signupEmail').val();
+				email = $.trim(email);
+				if (email == '') {
+					$('#email-info').html('<span class="bg-info" style="color:black; padding:3px;">Please provide your email id</span>');
+					return false;
+				} else {
+					$.post('php/email_check.php', {
+						'signupEmail': email
+					}, function(data) {
+						$('#email-info').html(data);
+					});
+					return true;
+				}
+			}
+
+			$('#signUpBtn').click(function() {
+				check_email();
+				var p = $('#password1').val();
+				var cp = $('#password2').val();
+				if (p == '' || cp == '') {
+					$('#password-info').html('Please provide a password');
+				} else {
+					if (p == cp && check_email()) {
+						//sing up
+						loader.show();
+						var d = $('#sign_up_form').serialize();
+						$.ajax({
+							url: "php/signup.php",
+							method: "POST",
+							data: d,
+							success: function(data) {
+								$('#sign_up_form').hide();
+								$('#success').show();
+								$('#success').html('<h1 style="color:#78ffee;">' + data + '</h1>');
+							},
+							error: function() {
+								alert('There is some problem we are facing.');
+							},
+							complete: function() {
+								loader.hide();
+							}
+						});
+					} else {
+						$('#password-info').html('<b>Password did not matched. Please try again.</b>');
+					}
+				}
+				return false;
+			});
+
+			$('#loginBtn').click(function() {
+				var logdata = $('#login_form').serialize();
+
+				$.ajax({
+					url: 'php/login.php',
+					method: "post",
+					data: logdata,
+					success: function(data) {
+						$('#pass_err').html(data);
+					},
+					error: function() {
+						alert('There is some problem we are facing.');
+					},
+					complete: function() {}
+				});
+				return false;
+			});
+
+
+			$(function() {
+				$('[data-toggle="tooltip"]').tooltip();
+			});
+
+			$(function() {
+				$('[data-toggle="popover"]').popover();
+			});
+		</script>
+	</body>
+
+	</html>
